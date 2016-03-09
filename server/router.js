438048786ce86b53fd61e94e38554125857f1f3b
hellowego <hellowego@gmail.com>
2016-03-09 17:11:56 +0800

    增加map功能

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 var config = require('./config');
+var map = require('./util/map');
 
 var name = '{	"first_name" : "hi",	"last_name" : "hello"}';
 
@@ -7,8 +8,13 @@ function routerParse(config) {
 	// console.log(config);
 	// console.log(config.port);
 	// console.log(config.routers);
+
+	var routerMap = new map();
+
 	var configstr = JSON.stringify(config);
 	var contact = JSON.parse(configstr);
+	// console.log(configstr);
+
 	for (var key in contact) {
 		if (key == 'port') {
 			console.log('%s, %s ', key, contact[key]);
@@ -16,23 +22,31 @@ function routerParse(config) {
 		if (key == 'routers') {
 			//console.log('%s, %s ', key, contact[key]);
 			var routers = contact[key];
-			for(var i = 0; i < routers.length; i++){
+			for (var i = 0; i < routers.length; i++) {
+
+				routerMap.put(routers[i]['surl'], routers[i]);
 				console.log(routers[i]);
-				for(var elem in routers[i]){
+				for (var elem in routers[i]) {
 					console.log('%s, %s ', elem, routers[i][elem]);
 				}
 			}
 		}
 	}
-	// console.log(configstr);
+
+	// print map
+	console.log('%s, %s', '/hi', routerMap.get('/hi')['fields']);
 }
 
-console.log('hi');
+function say(words) {
+	console.log(words);
+}
 
 function execute(someFunction, value) {
 	someFunction(value);
 }
 
-//execute(routerParse, "Hello");
+//execute(say, "Hello");
 
 routerParse(config);
+
+console.log('hi');
