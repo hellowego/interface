47460bd3e9aa4f1ff2568f38db01dc1cac5258cb
hellowego <hellowego@gmail.com>
2016-04-05 17:04:08 +0800

    修改配置文件

--- config/config.js
+++ config/config.js
@@ -1,22 +1,66 @@
-// 参数配置文件
 module.exports  = { 
-	routers : [
+	"routers" : [
 		{ 
-			proxyPath : "/hello",
-			proxyPort: 8801,
-			targetPath : "/hello",  
-			targetHost: "localhost",  
-			targetPort : 8888, 
-			fields : ["cardno", "trademoney"]
+			"proxyPath" : "/hello",
+			"proxyPort": 8081,
+			"targetPath" : "/hello",  
+			"targetHost": "localhost",  
+			"targetPort" : 8888, 
+			"fields" : ["cardno", "trademoney"]
 		} ,
 		{ 
-			proxyPath : "/hi",
-			proxyPort: 8802, 
-			targetPath : "/hello",  
-			targetHost: "localhost",  
-			targetPort : 8888, 
-			fields : ["cardno", "trademoney"]
+			"proxyPath" : "/hi",
+			"proxyPort": 8802, 
+			"targetPath" : "/hello",  
+			"targetHost": "localhost",  
+			"targetPort" : 8888, 
+			"fields" : ["cardno", "trademoney"]
 		} 
+	],
+	"httpServers" : [
+		{
+			"url" : "/hello",
+			"port": 8081,			
+			"destinationNo" : "0001",
+			"status" : "1",
+			"des" : ""
+		},
+		{
+			"utl" : "/hello",
+			"port": 8082,
+			"destinationNo" : "0001",
+			"status" : "1",
+			"des" : ""			
+		}
+	],
+	
+	"socketServers" : [
+		{
+			"host" : "0.0.0.0",
+			"Port": 9091,
+			"destinationNo": "0001",
+			"status" : "1",
+			"des" : ""
+		}
+	],
+	
+	"destinations" : [
+		{
+			"no" : "0001"	,
+			"type": "http",
+			"host": "0.0.0.0",
+			"port" : 8083, 
+			"status" : "1",
+			"des" : ""
+		},
+		{
+			"no" : "0002"	,
+			"type": "socket",
+			"host": "0.0.0.0",
+			"port" : 9092, 
+			"status" : "1",
+			"des" : ""
+		}
 	]
 };
  
