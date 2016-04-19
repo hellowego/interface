4292167a9a7f671f9589b1b2617e01915fb783d3
hellowego <hellowego@gmail.com>
2016-04-19 17:38:08 +0800

    用eventproxy同步

--- sample/socket/socketServer.js
+++ sample/socket/socketServer.js
@@ -15,31 +15,45 @@ net.createServer(function (sock) {
 	// 我们获得一个连接 - 该连接自动关联一个socket对象
 	console.log('CONNECTED: ' +
 		sock.remoteAddress + ':' + sock.remotePort);
-		
-	
-		
+
 	var ep = EventProxy();
 	ep.fail(function (err) {
-		console.log('11111111111111111111111111111111111111: ');
-		throw err;
+		console.log('ep.fail: %s', err.message);
+		// throw err;
 	});
-	
-	function myfail(){
-		console.log('11111111111111111111111111111111111111: ');
+
+	function myfail() {
+		console.log('myfail: ');
 	}
+
+	// 获取socket客户端发来的数据
+	ep.on('fetchReqData', fetchReqData);
 	
-	ep.once('fetchReqData', fetchReqData);
-	ep.once('proxyRequest', proxyRequest).fail(myfail);;
+	// 向代理服务器发送数据
+	ep.on('proxyRequest', proxyRequest);
 	
+	// 代理服务器返回数据
+	ep.on("proxyResponse", proxyResponse);
+	
+	// 定义异常
+	ep.fail(myfail);
+
 	function fetchReqData(result, p2) {
 		console.log('---fetchReqData---');
 		console.log(result);
 		console.log(postData);
 		console.log(p2);
-		
+
 		ep.emit("proxyRequest");
 	}
 	
+	function proxyResponse(result) {
+		console.log('---proxyResponse---');
+		console.log(result);
+		console.log(postData);
+		console.log(p2);		
+	}
+
 	function proxyRequest() {
 		console.log('---proxyRequest---');
 		var options = {
@@ -73,35 +87,28 @@ net.createServer(function (sock) {
 				});
 				res.on("end", function () {
 					var serverResData;
-					async.series([
-							//报文处理
-							//发送代理报文
-							function (callback) {
-								console.log(resData);
-								serverResData = Buffer.concat(resData);
-								console.log(serverResData);
-								
-								try {
-									var j1 = JSON.parse(serverResData + "123");
-									console.log(j1);
-								} catch (err)
-								{
-									console.log(err);
-									// throw(err);
-								}
-								
-								console.log("begin check");
-								console.log(serverResData.toString());
-								sock.write(serverResData.toString());
-								// proxyRsp.end(serverResData);
-								
-								console.log("check end");
-								callback();
-							}
-							//记录缓存或者持久化
-						], function (err, result) {
-						console.log('22222222222222222222222222222222: ');
-					});
+
+					console.log(resData);
+					serverResData = Buffer.concat(resData);
+					console.log(serverResData);
+
+					try {
+						var j1 = JSON.parse(serverResData );
+						console.log(j1);
+					} catch (err) {
+						console.log('catch error: ');
+						console.log(err);
+						// throw(err);
+						// ep.throw(err);
+					}
+
+					console.log("begin check");
+					console.log(serverResData.toString());
+					sock.write(serverResData.toString());
+					// proxyRsp.end(serverResData);
+
+					console.log("check end");
+					
 				});
 			});
 		proxyReq.on("error", function (e) {
@@ -110,7 +117,6 @@ net.createServer(function (sock) {
 		proxyReq.end(postData);
 	}
 
-	
 	sock.on('data', function (data) {
 		console.log('DATA ' + sock.remoteAddress + ': ' + data);
 		// 回发该数据，客户端将收到来自服务端的数据
@@ -121,14 +127,14 @@ net.createServer(function (sock) {
 		console.log(postData);
 		ep.emit("fetchReqData", postData.toString(), "hi");
 	});
-	
+
 	// 为这个socket实例添加一个"close"事件处理函数
 	sock.on('close', function (data) {
 
 		console.log('CLOSED: ' +
 			sock.remoteAddress + ' ' + sock.remotePort);
 	});
-	
-	
+
+	console.log("end");
 
 }).listen(PORT, HOST);
