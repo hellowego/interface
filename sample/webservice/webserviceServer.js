c272fde13e2102d30133a47342a46ce01c737587
euzhang <zhangyong2@lianchuang.com>
2016-04-25 09:20:22 +0800

    webservice demo

--- sample/webservice/webserviceServer.js
+++ sample/webservice/webserviceServer.js
@@ -1,37 +1,60 @@
 ﻿var soap = require('soap-server');
-
+var config = require('./wsdlServerConfig.js');
+var _ = require('lodash');
 function MyTestService() {
-}
-MyTestService.prototype.test1 = function (myArg1, myArg2) {
-    return myArg1 + myArg2;
-};
-//*
-MyTestService.prototype.test2 = function (myArg1, myArg2) {
-    return myArg1 + myArg2;
-};
-//*/
-function MyObject() {
-}
-MyObject.prototype.concated = '';
-MyObject.prototype.incremented = 0;
+    MyTestService.prototype.soapServer = function (InputArg) {
+        var jsonInput = JSON.parse(InputArg);
+        var webServerParams = _.find(config.webserviceServers,  {
+            "routeNo": jsonInput.routNo
+        });
+        console.log(InputArg.routNo);
+        if (!httpServerParams) {
+            next = false;
+            proxyRsp.writeHead(404);
+            proxyRsp.end();
+            console.log('404');
+        } else {
+            // 参数
+            requestInfo.httpServerParams = httpServerParams;
+            requestInfo.route = _.find(config.routes, {
+                'no' : httpServerParams.routeNo
+            });
+            requestInfo.response = _.find(config.response, {
+                'no' : httpServerParams.responseNo
+            });
+            console.log(requestInfo.resFields);
+            callback();
+        }
 
-MyTestService.prototype.test3 = function (strArg, intArg) {
-    var ret = new MyObject();
-    ret.concated = strArg + '[' + intArg + ']';
-    ret.incremented = intArg + 1;
-    return ret;
-};
+        var ret = new OutPutObject();
+        ret.values = inputArg;
+        
+        return ret;
+    };
+}
+
+
+
+function InputObejct(){
+}
+InputObejct.prototype.concated = '';
+InputObejct.prototype.incremented = '';
+function OutPutObject(){
+}
+OutPutObject.prototype.values = '';
+var soapOperation = soapService.getOperation('soapServer');
+soapOperation.setOutputType(OutPutObject, 'OutPutObject');
+
 var soapServer = new soap.SoapServer();
 var soapService = soapServer.addService('testService', new MyTestService());
-//*
+
+//soapOperation.setInputType(InputObejct, 'InputObejct');
+/*
 var test2operation = soapService.getOperation('test2');
 test2operation.setOutputType('number');
 test2operation.setInputType('myArg1', { type: 'number' });
 test2operation.setInputType('myArg2', { type: 'number' });
 //*/
-var test3operation = soapService.getOperation('test3');
-test3operation.setOutputType(MyObject, 'MyObject');
-test3operation.setInputType('intArg', { type: 'number' });
 
-
-soapServer.listen(1337, '127.0.0.1');
+soapServer.listen(1337, '127.0.0.1'); 
+console.log('Server running at http://127.0.0.1:1337/testService?wsdl');
---
+++ sample/webservice/wsdlServerConfig.js
@@ -0,0 +1,50 @@
+﻿module.exports = {
+    "webserviceServers": [{
+            "IP": "127.0.0.1",
+            "port": 8081,
+            "routeNo": "0101",
+            "responseNo" : "0102",
+            "checkFields": {
+                "OrigDomain": {
+                    "length": 8,
+                    "number": 0
+                },
+                "Token": {
+                    "length": 32,
+                    "number": 0
+                },
+                "CarNo" : {
+                    "length": 8,
+                    "number": 0
+                },
+                "Time" : {
+                    "length": 8,
+                    "number": 0
+                }
+            },	
+            "status": "1",
+            "des": ""
+        },
+        {
+            "IP": "127.0.0.1",
+            "port": 8081,
+            "routeNo": "0201",
+            "responseNo" : "0202",		
+            "checkFields": {
+                "OrigDomain": {
+                    "length": 8,
+                    "number": 0
+                },
+                "Token": {
+                    "length": 32,
+                    "number": 0
+                },
+                "DriverEmployeeID" : {
+                    "length": 8,
+                    "number": 0
+                }
+            },	
+            "status": "1",
+            "des": ""
+        }]
+}
